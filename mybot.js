f1ec71d3e8c51071637608139342aab016bf379e
dorkyoreopone <38141627+dorkyoreopone@users.noreply.github.com>
2018-04-06 18:21:41 -0400

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 function doMagic8BallVoodoo() {
-    var rand = [':8ball: Absolutly.', ':8ball: Absolutly not.', ':8ball: It is true.', ':8ball: Impossible.', ':8ball: Of course.', ':8ball: I do not think so.', ':8ball: It is true.', ':8ball: It is not true.', ':8ball: I am very undoubtful of that.', ':8ball: I am very doubtful of that.', ':8ball: Sources point to no.', ':8ball: Theories prove it.', ':8ball: Reply hazy try again', ':8ball: Ask again later', ':8ball: Better not tell you now', ':8ball: Cannot predict now', ':8ball: Concentrate and ask again'];
+    var rand = [':reduno: 1!', ':reduno: 2!', ':reduno: 3!' ':reduno: 4!', ':reduno: 5!', ':reduno: 6!', ':reduno: 7!', ':reduno: 8!', ':reduno: 9!', ':reduno: Reverse!', ':reduno: Skip!', ':reduno: Draw 2!', ':reduno: Draw 4!', ':blueuno: 1!', ':blueuno: 2!', ':blueuno: 3!' ':blueuno: 4!', ':blueuno: 5!', ':blueuno: 6!', ':blueuno: 7!', ':blueuno: 8!', ':blueuno: 9!', ':blueuno: Reverse!', ':blueuno: Skip!', ':blueuno: Draw 2!', ':blueuno: Draw 4!', ':greenuno: 1!', ':greenuno: 2!', ':greenuno: 3!' ':greenuno: 4!', ':greenuno: 5!', ':greenuno: 6!', ':greenuno: 7!', ':greenuno: 8!', ':greenuno: 9!', ':greenuno: Reverse!', ':greenuno: Skip!', ':greenuno: Draw 2!', ':greenuno: Draw 4!', ':yellowuno: 1!', ':yellowuno: 2!', ':yellowuno: 3!' ':yellowuno: 4!', ':yellowuno: 5!', ':yellowuno: 6!', ':yellowuno: 7!', ':yellowuno: 8!', ':yellowuno: 9!', ':yellowuno: Reverse!', ':yellowuno: Skip!', ':yellowuno: Draw 2!', ':yellowuno: Draw 4!', ':wildcarduno: Wild Card! ***Spoooooky~!***'];
 
     return rand[Math.floor(Math.random()*rand.length)];
 }
@@ -30,7 +30,7 @@ client.on("message", (message) => {
 });
 
 client.on("message", (message) => {
-  if (message.content.startsWith("!8ball")) {
+  if (message.content.startsWith("!uno")) {
     message.channel.send(doMagic8BallVoodoo());
   }
 });

325616f4258e9bd99998e31e8c1e068310185653
dorkyoreopone <38141627+dorkyoreopone@users.noreply.github.com>
2018-04-06 15:34:18 -0400

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -23,9 +23,4 @@ client.on("message", (message) => {
   }
 });
 
-bot.on('serverNewMember',function (user, userID, channelID, message, evt){
-if(user === bot.servers.get('id',"SERVERID"))
-bot.sendMessage(channelID.get('name','general'),"Welcome to my Server "+user.mention()+"!")
-});
-
 client.login(process.env.BOT_TOKEN);

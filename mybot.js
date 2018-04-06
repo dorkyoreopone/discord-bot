1a493564b04f7d74810e841b9c3223776bd20ab9
dorkyoreopone <38141627+dorkyoreopone@users.noreply.github.com>
2018-04-06 19:20:38 -0400

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 function doMagic8BallVoodoo() {
-    var rand = ['\:reduno: One!!', '\:reduno: Two!'];
+    var rand = ['<:reduno:431938212760387594>: One!!', '<:reduno:431938212760387594>: Two!'];
 
     return rand[Math.floor(Math.random()*rand.length)];
 }

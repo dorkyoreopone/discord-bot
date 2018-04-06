f056a28cee4dbe55ac4994cc4d1579f296c9485e
dorkyoreopone <38141627+dorkyoreopone@users.noreply.github.com>
2018-04-06 17:44:01 -0400

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -1,6 +1,12 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+function doMagic8BallVoodoo() {
+    var rand = [':8ball: Absolutly.', ':8ball: Absolutly not.', ':8ball: It is true.', ':8ball: Impossible.', ':8ball: Of course.', ':8ball: I do not think so.', ':8ball: It is true.', ':8ball: It is not true.', ':8ball: I am very undoubtful of that.', ':8ball: I am very doubtful of that.', ':8ball: Sources point to no.', ':8ball: Theories prove it.', ':8ball: Reply hazy try again', ':8ball: Ask again later', ':8ball: Better not tell you now', ':8ball: Cannot predict now', ':8ball: Concentrate and ask again'];
+
+    return rand[Math.floor(Math.random()*rand.length)];
+}
+
 client.on("ready", () => {
   console.log("I am ready!");
 });
@@ -20,6 +26,12 @@ client.on("message", (message) => {
 client.on("message", (message) => {
   if (message.content.startsWith("!pancake")) {
     message.channel.send(":pancakes: omnom!");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!8ball")) {
+    message.channel.send(doMagic8BallVoodoo());
   }
 });
 

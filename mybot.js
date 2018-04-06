554f2c0049a5b1265aa5368fe235a744eaedf560
dorkyoreopone <38141627+dorkyoreopone@users.noreply.github.com>
2018-04-06 19:38:51 -0400

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 function doMagic8BallVoodoo() {
-    var rand = ['<:reduno:431938212760387594>: Draw 2! Ehhh.. could be worse, I guess?', '<:reduno:431938212760387594>: Draw 4! ***Sad Trombone Noises...***'];
+    var rand = ['<:blueuno:431938301469917209>: Skip! Skip blip dibbly bip!', '<:reduno:431938212760387594>: Skip! That either helped or harmed the person after you... lets hope its not the latter if you guys arent friends, right?', '<:reduno:431938212760387594>: Reverse! Hope the person before you was ready for that...', '<:reduno:431938212760387594>: Draw 2! Ehhh.. could be worse, I guess?', '<:blueuno:431938301469917209>: Draw 2! Sorry if you were close to an Uno...', '<:blueuno:431938301469917209>: Draw 4! Sheesh. Thats gonna hurt the soul a little, huh?', '<:blueuno:431938301469917209>: Reverse! The tables have been turned! And I mean that literally...', '<:reduno:431938212760387594>: Draw 4! ***Sad Trombone Noises...***'];
 
     return rand[Math.floor(Math.random()*rand.length)];
 }
